eba2f3b45ecd2a4d29f6ed5b1c73f0c618ff92bf
MinTuVumi <mdmozammlhossain10@gmail.com>
2026-06-15 18:28:22 +0600

    update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,16 @@ import LayoutClient from "@/components/LayoutClient";
 import Footer from "@/components/layout/Footer";
 import Providers from "./providers";
 
+export const metadata = {
+  title: "SeeERP",
+  description: "Business Management Software",
+  icons: {
+    icon: "/logo/icon-1.png",
+    shortcut: "/logo/icon-1.png",
+    apple: "/logo/icon-1.png",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -11,7 +21,6 @@ export default function RootLayout({ children }) {
           <LayoutClient>
             <div className="min-h-screen flex flex-col">
               <div className="flex-1">{children}</div>
-
               <Footer />
             </div>
           </LayoutClient>
